7c9998233e28f3c91901612a49b5c3552f74757d
rocketmonster32 <cloudwolf09@gmail.com>
2020-02-01 14:35:23 -0600

    Error correction

--- Assets/Jonathan/InventoryItem.cs
+++ Assets/Jonathan/InventoryItem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public interface IInventoryItem
+public class IInventoryItem
 {
     string Name { get; }
     Sprite Image { get; }
