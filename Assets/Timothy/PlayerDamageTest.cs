25b5c9f8119e1588ba4ac346d6de0597c33978b9
Timothy <timothy.wakefiel@smail.astate.edu>
2020-02-01 04:53:16 -0600

    Fixed Test Damage Volume

--- Assets/Timothy/PlayerDamageTest.cs
+++ Assets/Timothy/PlayerDamageTest.cs
@@ -6,63 +6,42 @@ using UnityEngine.UI;
 public class PlayerDamageTest : MonoBehaviour
 {
     //Flag used to tell if the object can be interacted with or not.
-    public bool isInteractable = false;
-
+    //public bool isInteractable = false;
+    public float TestDamage;
     public Slider HealthBar;
     public float Health = 100;
 
-    private float currentHealth;
+    public float currentHealth;
 
     void Start()
     {
         currentHealth = Health;
     }
-
+    
     public void TakeDamage(float damage)
     {
+        damage = TestDamage;
         currentHealth -= damage;
-        HealthBar.value = currentHealth;
+        //HealthBar.value = currentHealth;
     }
-    void Update()
-    {
-        //Checks if the player is in the collider and also if the key is pressed.
-        if (isInteractable)
-        {
-            //personalized code can go in here when activated.
-            //power on/timer pause
-            //call function
-            //play audio "Power Restored"(function)
-            Debug.Log("Damage");
-        }
-    }
-
+    
     /// <summary>
     /// Is called when there is an object that enters the collider's borders.
     /// </summary>
     /// <param name="other"></param>
-    private void OnTriggerEnter(Collider other)
+    public void OnTriggerStay(Collider other)
     {
         //Compares the tag of the object entering this collider.
         if (other.tag == "Player")
         {
+            Debug.Log("trigger");
             //turns on interactivity 
-            isInteractable = true;
+            //isInteractable = true;
             //take player health
             TakeDamage(5);
-            Debug.Log("5");
+            Debug.Log(currentHealth);
+
         }
-    }
-    /// <summary>
-    /// Is called when there is an object that leaves the collider's borders.
-    /// </summary>
-    /// <param name="other"></param>
-    private void OnTriggerExit(Collider other)
-    {
-        //compares the tag of the object exiting this collider.
-        if (other.tag == "Player")
-        {
-            //turns off interactivity 
-            isInteractable = false;
-        }
+   
     }
 }
