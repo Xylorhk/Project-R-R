4275862d1f271048465281b1b1c48b03056e7cd6
rocketmonster32 <cloudwolf09@gmail.com>
2020-02-01 14:59:10 -0600

    Inventory changes

--- Assets/Jonathan/InventoryItem.cs
+++ Assets/Jonathan/InventoryItem.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IInventoryItem
+public interface IInventoryItem
 {
     string Name { get; }
     Sprite Image { get; }
