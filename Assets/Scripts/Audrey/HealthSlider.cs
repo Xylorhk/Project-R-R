c97f1956127da2e00ab6045ec6d593d675ec41f0
Dakota Jensen <dakotajensen04@gmail.com>
2020-02-02 13:04:42 -0600

    Continued on room logic. Oxygen depletion

---
+++ Assets/Scripts/Audrey/HealthSlider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthSlider : MonoBehaviour
+{
+    private Slider slider;
+    private Player player;
+
+    void Start()
+    {
+        slider = GetComponent<Slider>();
+        player = GameObject.Find("Player").GetComponent<Player>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        slider.value = player.currentHealth;
+    }
+}
