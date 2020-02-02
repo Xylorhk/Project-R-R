002b1dd5e53c44df6db5f417adabe2e077654929
Samuel <Xylorhksshadow.sw@gmail.com>
2020-02-02 14:00:01 -0600

    Adds Power off event on Elder enter

--- Assets/PowerOff.cs
+++ Assets/PowerOff.cs
@@ -2,23 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ElderAnims : MonoBehaviour
+public class PowerOff : StateMachineBehaviour
 {
-
-    public Animator Animator;
-
     public GameObject observationRoom;
 
-    void Start()
-    {
-        Animator = GetComponent<Animator>();
-    }
     private void OnTriggerEnter(Collider other)
     {
         //compares the tag of the object exiting this collider.
         if (other.tag == "Player")
         {
-            Animator.SetBool("EnterScene", true);
             observationRoom.GetComponent<Room>().isPowered = false;
         }
     }
