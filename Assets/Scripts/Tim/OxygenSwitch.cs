7562c87c6f03ea4e4508256921499d4af3b1eab4
Timothy <timothy.wakefiel@smail.astate.edu>
2020-02-02 00:12:52 -0600

    Oxygen Switch Added

---
+++ Assets/Scripts/Tim/OxygenSwitch.cs
@@ -0,0 +1,49 @@
+﻿// ToggleLight.cs
+// Turns the light component of this object on/off when the user presses and releases the `L` key.
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OxygenSwitch : MonoBehaviour
+{
+	public LifeSupport power;
+	Light light;
+	public Player player;
+
+
+	// Use this for initialization
+	void Start()
+	{
+		light = gameObject.GetComponentInChildren<Light>();
+		if (power.isPowered == true)
+		{
+			light.color = Color.blue;
+		}
+		else
+		{
+			light.color = Color.red;
+		}
+	}
+
+
+	public void OnMouseDown()
+	{
+		if (power.isPowered)
+		{
+			player.replenishOxygen()
+			power.TogglePower();
+			light.color = Color.red;
+			Debug.Log("Switch Occured off");
+		}
+
+		else
+		{
+			power.TogglePower();
+			light.color = Color.blue;
+			Debug.Log("Switch Occured on");
+			Debug.Log(power.isPowered);
+		}
+	}
+}
