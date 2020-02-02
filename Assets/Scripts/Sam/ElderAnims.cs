94a67d888290615bcbb6c0ce416f51868c6ad8a1
Samuel <Xylorhksshadow.sw@gmail.com>
2020-02-02 13:21:50 -0600

    Adds Enter animation

---
+++ Assets/Scripts/Sam/ElderAnims.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElderAnims : MonoBehaviour
+{
+
+    public Animator Animator;
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //compares the tag of the object exiting this collider.
+        if (other.tag == "Player")
+        {
+            Animator.SetBool("EnterScene", true);
+        }
+    }
+}
